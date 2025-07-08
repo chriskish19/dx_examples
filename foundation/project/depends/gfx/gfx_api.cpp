c1f5179073d6e99d15341cdc79d25723b0c5dd8b
chriskish19 <68289629+chriskish19@users.noreply.github.com>
2025-07-08 13:03:08 -0600

    a starting point
    a foundation for doing more example programs

---
+++ foundation/project/depends/gfx/gfx_api.cpp
@@ -0,0 +1,11 @@
+/***********************************************
+* File: gfx_api.cpp
+*
+* Purpose: all direct x headers compiled unit
+*
+* Project: core
+*
+************************************************/
+
+#include NAMES_INCLUDE
+#include GFX_INCLUDE_PATH
